5af871c17ab0f7a9cf9274ccc1e4aa9399538b70
Tomdatbenik <tomvankaathoven@gmail.com>
2019-12-16 16:02:09 +0100

    Boi and map

--- BloodRunV2/Assets/Scripts/UnityMonobehaivors/Player/PlayerMovement.cs
+++ BloodRunV2/Assets/Scripts/UnityMonobehaivors/Player/PlayerMovement.cs
@@ -6,17 +6,44 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public int speed;
+    public Animator Animator;
 
     private void FixedUpdate()
     {
+        Rotate();
         Move();
     }
 
     private void Move()
     {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        Debug.Log(Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            Animator.SetBool("Running", true);
+        }
+        else
+        {
+            Animator.SetBool("Running", false);
+        }
+
         rb.MovePosition(rb.position + new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxisRaw("Vertical") * Time.deltaTime * speed));
 
-        ConnectionManager.playerLogic.MovePlayerOnServer(CreatePlayerInfoFromTransform(rb.transform));
+        //ConnectionManager.playerLogic.MovePlayerOnServer(CreatePlayerInfoFromTransform(rb.transform));
+    }
+
+    private void Rotate()
+    {
+        if(Input.GetButton("RotateRight"))
+        {
+            rb.transform.Rotate(new Vector3(0, 4, 0));
+        }
+        else if (Input.GetButton("RotateLeft"))
+        {
+            rb.transform.Rotate(new Vector3(0, -4, 0));
+        }
+
     }
 
     private PlayerInfo CreatePlayerInfoFromTransform(Transform transform)
